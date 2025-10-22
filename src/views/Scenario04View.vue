0f6038cdba5a2a47d361879177fa3de48d79b9be
Sina Sangiji <58388732+cenaei@users.noreply.github.com>
2025-10-22 19:52:26 +0330

    Implement Scenario 04

---
+++ src/views/Scenario04View.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Scenario04View</h1>
+  <div>
+    <!-- <header>
+       <h1>Virtual List (Vue 3 + Composition API)</h1>
+      <p>No Libraries Used. Fixed heights for each item.</p>
+      <p>
+        Keep Only a few items in DOM for a very large list. Scroll below either
+        by dragging the scroll bar or by moving your mouse wheel.
+      </p>
+    </header> -->
+
+    <VirtualScroll />
+  </div>
+</template>
+
+<script setup>
+import VirtualScroll from "@/components/Scenario04/VirtualScroll.vue";
+</script>
+
+<style scoped></style>
