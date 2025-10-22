11b4ec25beffcb84998a265f37651d248165dd22
Sina Sangiji <58388732+cenaei@users.noreply.github.com>
2025-10-22 20:58:04 +0330

    Implement Scenario 06

--- src/App.vue
+++ src/App.vue
@@ -42,6 +42,12 @@ import { RouterLink, RouterView } from "vue-router";
           to="/sce05"
           >Scenario 05</RouterLink
         >
+        <RouterLink
+          class="bg-gray-300 px-4 py-2 rounded-2xl"
+          active-class="text-blue-800 rounded-3xl transition"
+          to="/sce06"
+          >Scenario 06</RouterLink
+        >
       </nav>
     </div>
   </header>
---
+++ src/views/Scenario06View.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="flex flex-col h-screen">
+    <header class=" bg-blue-100 px-4 py-2 text-center rounded-3xl">
+      <h1>Scenario06</h1>
+    </header>
+    <main class=" bg-blue-200 px-4 py-2 text-center rounded-3xl flex flex-col gap-2 flex-1 overflow-y-auto">
+      <p class=" bg-blue-300 rounded-3xl" v-for="n in 100" :key="n">Content {{ n }}</p>
+    </main>
+    <footer class=" bg-blue-100 px-4 py-2 text-center rounded-3xl">Copyright by Sina</footer>
+  </section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
